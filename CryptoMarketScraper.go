8652fc76634417b82a34a27b73b09214439b2e9c
Inderpreet Singh <inderpreet.singh@u.nud.edu>
2022-06-23 17:43:38 +0530

    Initial commit for all scraping done

---
+++ CryptoMarketScraper.go
@@ -0,0 +1,20 @@
+package GoLang_WEbScrapper_Colly
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+func CryptoExample() {
+	c := colly.NewCollector()
+
+	c.OnHTML("a.cmc-table__column-name--name", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://coinmarketcap.com/all/views/all/")
+}
